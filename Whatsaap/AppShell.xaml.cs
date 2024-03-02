1f63479bad6e3f3ddc85b6c30c018e7aa123c552
FaridMendoza22 <faridmendoza9310@hotmail.com>
2024-03-02 15:40:27 -0500

    Agregar archivos de proyecto.

---
+++ Whatsaap/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Whatsaap
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Whatsaap/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Whatsaap
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
